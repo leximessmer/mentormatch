b7a3afb214e6e2858e7b469307a0e5c669a72aeb
Raghu Betina <raghu@firstdraft.com>
2018-06-05 15:20:46 +0000

    Only job user can modify job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,14 @@
 class JobsController < ApplicationController
+  before_action :current_user_must_be_job_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_job_user
+    job = Job.find(params[:id])
+
+    unless current_user == job.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @jobs = Job.all
 
