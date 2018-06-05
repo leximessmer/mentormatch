2d02f39faf4d4e7fd345952fdce1157400666374
Raghu Betina <raghu@firstdraft.com>
2018-06-05 15:19:57 +0000

    Direct association between users and favorites

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,10 @@
 class Favorite < ApplicationRecord
   # Direct associations
 
+  belongs_to :user,
+             :foreign_key => "favoriter_id",
+             :counter_cache => true
+
   # Indirect associations
 
   # Validations
